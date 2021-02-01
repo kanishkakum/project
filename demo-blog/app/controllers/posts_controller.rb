c48ea64db4094b7a57248060f0ad09e8c6276d5a
kanishka kumar <kanishkakumar408@gmail.com>
2021-02-01 15:22:09 +0530

    created post model

--- demo-blog/app/controllers/posts_controller.rb
+++ demo-blog/app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+	
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
 end
