e9edf91952f854af5914ed4660561c54fbb52cc6
kanishka kumar <kanishkakumar408@gmail.com>
2021-02-15 13:14:18 +0530

    created a create action for creating new artcile, show action to show details about article on show.html.erb

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,23 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end	
+ 
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      flash[:notice]= "article created successfully"
+      redirect_to @article 
+    else
+      render 'new'
+    end    	
+  end	
 
+  def show
+    @article = Article.find(params[:id])
+  end	
 
+  private
+  def articles_params
+  	params.require(:article).permit(:title, :description)
+  end	
 end
