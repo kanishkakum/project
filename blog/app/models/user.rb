19da0f1539424514d4e4b9e1392aa373f9aa65c1
kanishka kumar <kanishkakumar408@gmail.com>
2021-02-15 17:42:34 +0530

    added foreign_key in article, applied association btw user & articles

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,4 +1,10 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, uniqueness: true
+class User < ActiveRecord::Base
+   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
+   format: { with: VALID_EMAIL_REGEX }
+   before_save { self.email = email.downcase }
+
+   has_many :articles
+
 end
